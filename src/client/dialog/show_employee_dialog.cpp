2afc1b6dd68cebe3237f5d558c3bb2a3af1652e9
waffencode <waffencode@gmail.com>
2023-02-24 00:51:13 +0300

    Change naming

--- src/client/dialog/show_employee_dialog.cpp
+++ src/client/dialog/show_employee_dialog.cpp
@@ -6,10 +6,10 @@ namespace client
 {
     namespace dialog 
     {
-        void show_employee_dialog(employee::employee_collection* collection)
+        void show_employee_dialog(employee::employee_collection* _collection)
         {
             std::cout << std::left << std::setw(15) << "First name" << std::setw(15) << "Last name" << std::setw(15) << "Salary per month" << std::endl;              
-            for (const auto& element : collection->collection)
+            for (const auto& element : _collection->collection)
             {
                 const employee::employee* current = element.get();
                 std::cout << std::left << std::setw(15) << current->get_name();
