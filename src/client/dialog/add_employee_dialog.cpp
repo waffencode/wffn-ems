7ad7389a144b6f5b3aac83246020217ababcccc5
waffencode <waffencode@gmail.com>
2023-02-28 20:03:44 +0300

    Fix nested namespaces

--- src/client/dialog/add_employee_dialog.cpp
+++ src/client/dialog/add_employee_dialog.cpp
@@ -2,23 +2,20 @@
 #include <iostream>
 #include <string>
 
-namespace client 
+namespace client::dialog
 {
-    namespace dialog 
+    void add_employee_dialog(employee::employee_collection *collection)
     {
-        void add_employee_dialog(employee::employee_collection* collection)
-        {
-            std::string name = "", surname = "";
-            float salary = 0;
+        std::string name = "", surname = "";
+        float salary = 0;
 
-            std::cout << "Enter employee first name: ";
-            std::cin >> name;
-            std::cout << "Enter employee last name: ";
-            std::cin >> surname;
-            std::cout << "Enter employee salary per month: ";
-            std::cin >> salary;
+        std::cout << "Enter employee first name: ";
+        std::cin >> name;
+        std::cout << "Enter employee last name: ";
+        std::cin >> surname;
+        std::cout << "Enter employee salary per month: ";
+        std::cin >> salary;
 
-            collection->add_employee(std::make_unique<employee::employee>(name, surname, salary));
-        }
+        collection->add_employee(std::make_unique<employee::employee>(name, surname, salary));
     }
 }
--- src/client/dialog/show_employee_dialog.cpp
+++ src/client/dialog/show_employee_dialog.cpp
@@ -2,26 +2,23 @@
 #include <iostream>
 #include <iomanip>
 
-namespace client 
+namespace client::dialog
 {
-    namespace dialog 
+    static const int FIELD_WIDTH = 15;
+
+    void show_employee_dialog(employee::employee_collection *_collection)
     {
-        static const int FIELD_WIDTH = 15;
+        std::cout << std::left << std::setw(FIELD_WIDTH / 2) << "ID" << std::setw(FIELD_WIDTH) << "First name" << std::setw(FIELD_WIDTH) << "Last name" << std::setw(FIELD_WIDTH) << "Salary per month" << std::endl;
+        size_t count = 0;
 
-        void show_employee_dialog(employee::employee_collection* _collection)
+        for (const auto &element : _collection->get_collection())
         {
-            std::cout << std::left << std::setw(FIELD_WIDTH / 2) << "ID" << std::setw(FIELD_WIDTH) << "First name" << std::setw(FIELD_WIDTH) << "Last name" << std::setw(FIELD_WIDTH) << "Salary per month" << std::endl;
-            size_t count = 0;
-
-            for (const auto& element : _collection->get_collection())
-            {
-                const employee::employee* current = element.get();
-                std::cout << std::left 
-                    << std::setw(FIELD_WIDTH / 2) << ++count
-                    << std::setw(FIELD_WIDTH) << current->get_name()
-                    << std::setw(FIELD_WIDTH) << current->get_surname()
-                    << std::setw(FIELD_WIDTH) << current->get_salary() << std::endl;
-            }
+            const employee::employee *current = element.get();
+            std::cout << std::left
+                      << std::setw(FIELD_WIDTH / 2) << ++count
+                      << std::setw(FIELD_WIDTH) << current->get_name()
+                      << std::setw(FIELD_WIDTH) << current->get_surname()
+                      << std::setw(FIELD_WIDTH) << current->get_salary() << std::endl;
         }
     }
 }
