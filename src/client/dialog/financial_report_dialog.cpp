ea7e6fe1f0268add476b50b13ef633ac07ed9f4b
waffencode <waffencode@gmail.com>
2023-03-26 00:58:46 +0300

    Add financial report dialog classes

---
+++ src/client/dialog/financial_report_dialog.cpp
@@ -0,0 +1,9 @@
+#include "financial_report_dialog.hpp"
+
+namespace client::dialog_functions 
+{
+    void financial_report_dialog()
+    {
+        
+    }
+}
---
+++ src/client/dialog/financial_report_dialog.hpp
@@ -0,0 +1,11 @@
+#ifndef FINANCIAL_REPORT_DIALOG_HPP
+#define FINANCIAL_REPORT_DIALOG_HPP
+
+#include "../../core/core.hpp"
+
+namespace client::dialog_functions
+{
+    void financial_report_dialog();
+}
+
+#endif
